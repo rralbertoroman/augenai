e27eaab49e9544d5869a6c7385e9d9be50734154
rralbertorman <rr.albertorman@gmail.com>
2025-10-10 17:15:30 -0400

    feat: Create the basic structure of the app

---
+++ apps/web/src/components/layout/dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard:React.FC = () => {
+    return (
+        <div className="grid grid-cols-2 grid-rows-2 h-full w-full bg-blue-400">
+            <div className="bg-red-300">Q4</div>
+            <div className="bg-green-300">Q1</div>
+            <div className="bg-yellow-300">Q3</div>
+            <div className="bg-blue-300">Q2</div>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ apps/web/src/components/layout/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer:React.FC = () => {
+    return (
+        <div className="flex flex-row items-center justify-between w-full h-fit px-5 py-4 bg-gradient-to-b from-gray-50 to-gray-200">
+        </div>
+    )
+}
+
+export default Footer;
