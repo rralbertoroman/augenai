59c513944a423076a6a0b515ad5d04e2a5f08f1d
rralbertorman <rr.albertorman@gmail.com>
2025-10-11 08:10:33 -0400

    feat: Add the footer

--- apps/web/src/components/layout/footer.tsx
+++ apps/web/src/components/layout/footer.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Footer:React.FC = () => {
     return (
-        <div className="flex flex-row items-center justify-between w-full h-fit px-5 py-4 bg-gradient-to-b from-gray-50 to-gray-200">
+        <div className="flex flex-row items-center justify-center w-full h-fit px-5 py-4 bg-gradient-to-b from-gray-50 to-gray-200">
+            <p>&copy; A-Eye 2025</p>
         </div>
     )
 }
