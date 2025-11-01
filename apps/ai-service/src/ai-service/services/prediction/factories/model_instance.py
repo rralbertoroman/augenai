094fcafe4cd67224aa6a35abf010132b713214ed
rralbertorman <rr.albertorman@gmail.com>
2025-11-01 02:58:06 -0400

    HACK-6 (feat):  Add the basics of the prediction service

---
+++ apps/ai-service/src/ai-service/services/prediction/factories/model_instance.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class ModelInstance(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def run(self, image):
+        pass
