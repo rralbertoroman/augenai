b67383ea5501a41845615a418b189af93fe5efa9
rralbertorman <rr.albertorman@gmail.com>
2025-11-01 02:58:06 -0400

    HACK-6 (feat):  Add the basics of the prediction service

---
+++ apps/ai-service/src/ai-service/services/prediction/model_pool.py
@@ -0,0 +1,22 @@
+
+
+class ModelPool:
+    _models_in_use = {}
+    _factories = {}
+
+    def __init__(self):
+        pass
+    
+    def _get_model_instance(self, model_id):
+        return self._factories.get(model_id) # ModelNotFound errors are handled on NextJS
+
+    def get_model(self, model_id):
+        
+        if model_id in self._models_in_use:
+            return self._models_in_use[model_id]
+        
+        model_factory = self._get_model_instance(model_id)
+        model = model_factory()
+
+        self._models_in_use[model_id] = model
+        return model
---
+++ apps/ai-service/src/ai-service/services/prediction/service.py
@@ -0,0 +1,8 @@
+
+
+class ModelService:
+    def __init__(self):
+        pass
+
+    def predict(self, image, model_id):
+        pass
