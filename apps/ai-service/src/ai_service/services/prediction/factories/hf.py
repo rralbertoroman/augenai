adf6eda7b36c37923cbe3aa306378c7dbf713c55
rralbertorman <rr.albertorman@gmail.com>
2025-11-03 07:09:31 -0500

    HACK-6 (feat):  Create the tests for the AI service

--- apps/ai-service/src/ai_service/services/prediction/factories/hf.py
+++ apps/ai-service/src/ai_service/services/prediction/factories/hf.py
@@ -2,22 +2,25 @@ import logging
 from pathlib import Path
 
 import torch
+from PIL.Image import Image
 from torch import nn
 from transformers import ViTForImageClassification, ViTImageProcessor
 
-from PIL.Image import Image
 from ai_service.config import settings
 from ai_service.models.schemas import PredictionResult
+
 from .model_instance import ModelInstance
 
 logger = logging.getLogger(__name__)
 
+
 def vit_clsf_model_factory(model_id: str):
     """Create a ViT model for image classification"""
+
     class HFViTModel(ModelInstance):
         def __init__(self, model_id):
             self.model_id = model_id
-            
+
             model_path = Path(settings.weights_dir) / self.model_id
             logger.info(f"Loading model from {model_path.resolve()}")
             self.model = ViTForImageClassification.from_pretrained(str(model_path))
@@ -29,7 +32,7 @@ def vit_clsf_model_factory(model_id: str):
 
         def run(self, image: Image):
             inputs = self.processor(images=image, return_tensors="pt")
-            
+
             logger.info(f"Running {self.model_id} inference on device: {self.device}")
             model_dev = self.model.to(self.device)
             inputs_dev = inputs.to(self.device)
@@ -41,26 +44,27 @@ def vit_clsf_model_factory(model_id: str):
             # Get predicted class and confidence
             predicted_class_idx = logits.argmax(-1).item()
             predicted_class = model_dev.config.id2label[predicted_class_idx]
-            confidence = nn.functional.softmax(logits, dim=-1)[0, predicted_class_idx].item()
+            confidence = nn.functional.softmax(logits, dim=-1)[
+                0, predicted_class_idx
+            ].item()
 
             # Create prediction result
             prediction_result = PredictionResult(
                 class_id=predicted_class_idx,
                 class_name=predicted_class,
                 confidence=confidence,
-                model_metadata={} # Add any relevant metadata here
+                model_metadata={},  # Add any relevant metadata here
             )
 
             logger.info(f"Prediction result: {prediction_result}")
-            
+
             del model_dev
             del inputs_dev
             del outputs
             del logits
 
             return prediction_result
-            
-    
+
     return HFViTModel(model_id)
 
 
--- apps/ai-service/src/ai_service/services/prediction/service.py
+++ apps/ai-service/src/ai_service/services/prediction/service.py
@@ -1,3 +1,8 @@
+from PIL.Image import Image
+
+from ai_service.models.schemas import PredictionResult
+from ai_service.services.prediction.factories.model_instance import ModelInstance
+
 from ..config import settings
 from .factories import hf
 from .model_pool import ModelPool
@@ -10,8 +15,8 @@ class ModelService:
         }
         self._model_pool = ModelPool(factories)
 
-    def predict(self, image, model_id):
-        model = self._model_pool.get_model(model_id)
+    def predict(self, image: Image, model_id: str) -> PredictionResult:
+        model: ModelInstance = self._model_pool.get_model(model_id)
 
         result = model.run(image)
         return result
