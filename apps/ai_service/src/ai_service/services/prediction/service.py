6d0411706dbb52d5f6f5f2a05711e345777d7968
rralbertorman <rr.albertorman@gmail.com>
2025-11-16 00:02:15 -0500

    HACK-41 refactor(ai_service): update the typing to allow returning multiple high confidence classes

--- apps/ai_service/src/ai_service/services/prediction/service.py
+++ apps/ai_service/src/ai_service/services/prediction/service.py
@@ -1,11 +1,14 @@
+import logging
+
 from PIL.Image import Image
 
 from ai_service.models.schemas import PredictionResult
 from ai_service.services.prediction.factories.model_instance import ModelInstance
 
-from ..config import settings
 from .factories import hf
 from .model_pool import ModelPool
+
+logger = logging.getLogger(__name__)
 
 
 class PredictionService:
@@ -14,9 +17,11 @@ class PredictionService:
             "diabetic-retinopathy-224-procnorm-vit": hf.vit_clsf_model_factory,
         }
         self._model_pool = ModelPool(factories)
+        logger.info("Prediction service initialized")
 
     def predict(self, image: Image, model_id: str) -> PredictionResult:
         model: ModelInstance = self._model_pool.get_model(model_id)
+        logger.info(f"Model with id '{model_id}' is ready for prediction")
 
         result = model.run(image)
         return result
