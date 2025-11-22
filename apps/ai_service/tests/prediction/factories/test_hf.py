a26b8ede6a72a50f2130147de25173a0007ba07f
rralbertorman <rr.albertorman@gmail.com>
2025-11-22 13:05:21 -0500

    HACK-61 feat(ai_service): Add the factory and unit tests for the new model

--- apps/ai_service/tests/prediction/factories/test_hf.py
+++ apps/ai_service/tests/prediction/factories/test_hf.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import pytest
 from ai_service.services.prediction.factories import hf
 
 logger = logging.getLogger(__name__)
@@ -7,8 +8,27 @@ logger = logging.getLogger(__name__)
 
 # Test class for HuggingFace factories
 class TestFactoriesHF:
-    def test_vit_clsf_model_factory(self, sample_images):
-        model = hf.vit_clsf_model_factory("diabetic-retinopathy-224-procnorm-vit")
+    @pytest.mark.parametrize("model_id", ["diabetic-retinopathy-224-procnorm-vit"])
+    def test_vit_clsf_model_factory(self, sample_images, model_id):
+        model = hf.vit_clsf_model_factory(model_id)
+        assert model is not None
+
+        for image in sample_images:
+            result = model.run(image)
+            assert result is not None
+            assert result.metadata.inference_time_ms > 0
+            assert result.metadata.model_version == "1"
+            assert len(result.predictions) > 0
+
+            logger.info(f"===Result: {result}")
+
+            logger.info(
+                f"===Result Dict: \n\n{json.dumps(result.model_dump(), indent=4)}"
+            )
+
+    @pytest.mark.parametrize("model_id", ["swinv2_tiny_for_glaucoma_classification"])
+    def test_swinv2_clsf_model_factory(self, sample_images, model_id):
+        model = hf.swinv2_clsf_model_factory(model_id)
         assert model is not None
 
         for image in sample_images:
