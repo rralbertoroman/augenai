62ca90049616ec4541cd84356db00ad955ded24d
rralbertorman <rr.albertorman@gmail.com>
2025-11-24 23:44:36 -0500

    HACK-73 feat(ai_service): create the basic test for the factory and fix the import error

--- apps/ai_service/src/ai_service/services/prediction/factories/__init__.py
+++ apps/ai_service/src/ai_service/services/prediction/factories/__init__.py
@@ -1,10 +1,10 @@
 from .hf import swinv2_clsf_model_factory, vit_clsf_model_factory
-from .ultralytics import yolo_detection_model_factory
+from .ultralytics import ultralytics_detection_factory
 from .model_instance import ModelInstance
 
 __all__ = [
     "swinv2_clsf_model_factory",
     "vit_clsf_model_factory",
-    "yolo_detection_model_factory",
+    "ultralytics_detection_factory",
     "ModelInstance",
 ]
