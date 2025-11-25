a476e1b6c5d1053fb66615c0578f327832f571b2
rralbertorman <rr.albertorman@gmail.com>
2025-11-24 23:32:36 -0500

    HACK-73 feat(ai_service): expose the factories for YOLO and Swin Transformer model

--- apps/ai_service/src/ai_service/services/prediction/factories/__init__.py
+++ apps/ai_service/src/ai_service/services/prediction/factories/__init__.py
@@ -1,7 +1,10 @@
-from .hf import vit_clsf_model_factory
+from .hf import swinv2_clsf_model_factory, vit_clsf_model_factory
+from .ultralytics import yolo_detection_model_factory
 from .model_instance import ModelInstance
 
 __all__ = [
+    "swinv2_clsf_model_factory",
     "vit_clsf_model_factory",
+    "yolo_detection_model_factory",
     "ModelInstance",
 ]
