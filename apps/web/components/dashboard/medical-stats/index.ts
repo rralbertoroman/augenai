986501b2ad787637ce34b75653229a80a9456480
rralbertoroman <rralbertoroman@gmail.com>
2025-11-26 10:27:49 -0500

    HACK-78 refactor(web): delete the unnecessary index file

--- apps/web/components/dashboard/medical-stats/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./MedicalStats";
-export * from "./types";
-export * from "./constants";
-export * from "./utils";
-
-export * from "./components/BaseStackedBar";
-export * from "./components/CustomTooltip";
-export * from "./components/DiseaseCohortChart";
-export * from "./components/StageTotalChart";
