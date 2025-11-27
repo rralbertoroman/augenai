374f96bfff5b4fa91595aa21558a75ace047fd2b
Rey <reynaldosuarezprieto@gmail.com>
2025-11-27 15:45:05 -0500

    feat: prediction requests improve

--- apps/web/server/zod-schemas/prediction_request.ts
+++ apps/web/server/zod-schemas/prediction_request.ts
@@ -1,4 +1,12 @@
 import { z } from "zod";
+import { type InferSelectModel } from "drizzle-orm";
+import {
+  PredictionsTable,
+  ClassificationsTable,
+  DetectionsTable,
+  ClassificationFeedbackTable,
+  DetectionFeedbackTable,
+} from "../db/schemas";
 
 export const CreatePredictionRequestSchema = z.object({
   patientId: z.uuid(),
@@ -38,3 +46,19 @@ export type GetPredictionRequestByIdInput = z.input<
 
 // OUTPUT TYPES
 export type PredictionRequestDTO = z.output<typeof PredictionRequestDTOSchema>;
+
+// DB TYPES
+type Classification = InferSelectModel<typeof ClassificationsTable>;
+type Detection = InferSelectModel<typeof DetectionsTable>;
+type ClassificationFeedback = InferSelectModel<
+  typeof ClassificationFeedbackTable
+>;
+type DetectionFeedback = InferSelectModel<typeof DetectionFeedbackTable>;
+type Prediction = InferSelectModel<typeof PredictionsTable>;
+
+export type PredictionWithTasks = Prediction & {
+  classifications: (Classification & {
+    feedbacks?: ClassificationFeedback[];
+  })[];
+  detections: (Detection & { feedbacks?: DetectionFeedback[] })[];
+};
