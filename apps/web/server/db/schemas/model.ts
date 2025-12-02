fc5659dc9b02bda80de417aa7f2ef4247a154df0
Rey <reynaldosuarezprieto@gmail.com>
2025-12-02 00:58:30 -0500

    feat: set supabase timezone

--- apps/web/server/db/schemas/model.ts
+++ apps/web/server/db/schemas/model.ts
@@ -9,7 +9,7 @@ const ModelsTable = pgTable("models", {
   modelTasks: text("model_tasks").array().notNull(),
   diseases: text("diseases").array().notNull(),
   acceptedImageTypes: text("accepted_image_types").array().notNull(),
-  latestTraining: timestamp("latest_training").notNull(),
+  latestTraining: timestamp("latest_training", { withTimezone: true, mode: "date" }).notNull(),
   accuracy: real("accuracy").notNull(),
   size: real("size").notNull(), // Model size in MB
   params: real("params").notNull(), // Number of parameters in millions
