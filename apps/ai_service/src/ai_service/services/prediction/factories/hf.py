f12aa634f21f0f391781457e50bba1d3ca47d854
rralbertoroman <rr.albertoroman@gmail.com>
2025-12-03 12:00:39 -0500

    HACK-120 feat(ai_service): Create and register the factory of DINOv2 for classification

--- apps/ai_service/src/ai_service/services/prediction/factories/hf.py
+++ apps/ai_service/src/ai_service/services/prediction/factories/hf.py
@@ -9,6 +9,7 @@ from transformers import (
     AutoImageProcessor,
     Swinv2ForImageClassification,
     ViTForImageClassification,
+    Dinov2ForImageClassification,
     ViTImageProcessor,
     PreTrainedModel,
 )
@@ -214,6 +215,58 @@ def vit_clsf_model_factory(model_id: str):
             return prediction_result
 
     return HFViTModel(model_id)
+
+
+def dinov2_clsf_model_factory(model_id: str):
+    """Create a Dinov2 model for image classification"""
+
+    class HFDinov2Model(ModelInstance):
+        def __init__(self, model_id: str, timeout: int = 60):
+            super().__init__(model_id=model_id, timeout=timeout)
+
+            model_path = Path(settings.weights_dir) / self.model_id
+            logger.info(f"Loading {self.model_id} model from {model_path.resolve()}")
+            self.model = Dinov2ForImageClassification.from_pretrained(str(model_path))
+            self.processor = AutoImageProcessor.from_pretrained(str(model_path))
+            self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+            logger.info(f"Loaded \n\n{'==' * 20}\n\n {self.model} \n\n{'==' * 20}")
+
+        def run(self, image: Image):
+            inputs = self.processor(images=image, return_tensors="pt")
+
+            logger.info(f"Running {self.model_id} inference on device: {self.device}")
+            model_dev = self.model.to(self.device)
+            inputs_dev = inputs.to(self.device)
+
+            start_time = datetime.now()
+            with torch.no_grad():
+                outputs = model_dev(**inputs_dev)
+                logits = outputs.logits
+
+            # time in milliseconds
+            inference_time_ms = (datetime.now() - start_time).total_seconds() * 1000
+
+            # Process logits and create prediction result
+            prediction_result = process_classification_output(
+                logits=logits,
+                model=model_dev,
+                inference_time_ms=inference_time_ms,
+            )
+
+            logger.info(f"Prediction result: {prediction_result}")
+
+            del model_dev
+            del inputs_dev
+            del outputs
+            del logits
+
+            return prediction_result
+
+    return HFDinov2Model(model_id)
+
+
+
 
 
 if __name__ == "__main__":
--- apps/ai_service/tests/prediction/factories/test_hf.py
+++ apps/ai_service/tests/prediction/factories/test_hf.py
@@ -43,3 +43,21 @@ class TestFactoriesHF:
             logger.info(
                 f"===Result Dict: \n\n{json.dumps(result.model_dump(), indent=4)}"
             )
+
+    @pytest.mark.parametrize("model_id", ["Diabetic_RetinoPathy_detection"])
+    def test_dinov2_clsf_model_factory(self, sample_images, model_id):
+        model = hf.dinov2_clsf_model_factory(model_id)
+        assert model is not None
+
+        for image in sample_images:
+            result = model.run(image)
+            assert result is not None
+            assert result.metadata.inference_time_ms > 0
+            assert result.metadata.model_version == "1"
+            assert len(result.predictions) > 0
+
+            logger.info(f"===Result: {result}")
+
+            logger.info(
+                f"===Result Dict: \n\n{json.dumps(result.model_dump(), indent=4)}"
+            )
